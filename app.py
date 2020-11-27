264ec5cee0b07fba530adfc687219477632c6c09
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2020-11-27 23:31:56 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-s = 'What is the answer?'
-print(len(s))
-t = 42
-number = int(t)
-u = str(number)
-print(s*3)
-print(s+''+u)
+def summ(first, second):
+    result =int(first)+int(second)
+    return result
+
+print("F"+str(summ(1, 12)))
